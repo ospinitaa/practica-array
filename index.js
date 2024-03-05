9fc49b6b665bf01bee61baa10d532dc745398fe8
SRospina <felipeospina026@gmail.com>
2024-03-05 16:16:37 -0500

    creamos la rama elementos_diferentes-tipos-datos

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 let arrayVacio = [];
 let miArray = ["elemento1", "elemento2", "elemento3"];
 console.log(miArray);
+
+// elementos de diferentes tipos de datos
+let arrayDiverso = ["texto", 123, true, { nombre: "Juan", edad: 30 }, ["mora", "fresa", "naranja"]];
+console.log(arrayDiverso);
