13d593d05a83b28e00702db5ec2b3139cec3170c
SRospina <felipeospina026@gmail.com>
2024-03-05 16:26:31 -0500

    creamos la rama construcor array

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ console.log(arrayDiverso);
 // utilizando el constructor Array()array con una secuencia numérica usando el método Array.from()
 let secuenciaNumerica = Array.from({ length: 10 }, (_, index) => index);
 console.log(secuenciaNumerica);
+
+//Crear un array utilizando el constructor Array()
+let miArray2 = new Array("elemento1", "elemento2", "elemento3");
+console.log(miArray);
