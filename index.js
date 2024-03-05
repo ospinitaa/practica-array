d3af2eb814d47648f3b0f70715df25f76e875777
SRospina <felipeospina026@gmail.com>
2024-03-05 16:23:21 -0500

    creamos la rama secuencia numerica

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ console.log(miArray);
 // elementos de diferentes tipos de datos
 let arrayDiverso = ["texto", 123, true, { nombre: "Juan", edad: 30 }, ["mora", "fresa", "naranja"]];
 console.log(arrayDiverso);
+
+// utilizando el constructor Array()array con una secuencia numérica usando el método Array.from()
+let secuenciaNumerica = Array.from({ length: 10 }, (_, index) => index);
+console.log(secuenciaNumerica);
